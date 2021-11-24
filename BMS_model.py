0d025e8a46263b5d38f2375a1d39189fcbd80478
Pozharkova <pozharkova.ao@phystech.edu>
2021-11-24 17:46:40 +0300

    added main.

---
+++ BMS_model.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+# license: GPLv3
+import math
+
+def move_body(body, surface, g, dt, max_x):
+    """Вычисляет проекции равнодействующей на тело силы, определяет ускорения скорости.
+    Перемещает тело. Производит необходимые коррекции
+    """
+
+    body.Fx = body.Fy = 0
+    x0 = body.x
+    y0 = body.y
+    r = body.r
+    #Определение точки касания телом поверхности
+    alpha = surface.find_alpha(x0)
+    x = math.sin(alpha) * r + x0
+    y = surface.find_y(x)
+    #Если тело касается поверхности, то определяются силы, действующие на него при движении
+    if  abs(r ** 2 - ((x0 - x) ** 2 + (y0 - y) ** 2)) <= 0.02:
+        F = -body.m * g * math.sin(alpha - math.pi)
+        kf = surface.find_friction(x)
+        if alpha != 0:
+            Ftp = kf * abs(F / math.tan(alpha - math.pi))
+        else:
+            Ftp = kf * body.m * g 
+        if body.Vx > 0:
+            F += Ftp
+        elif body.Vx < 0:
+            F -= Ftp
+        body.Fx += F * math.cos(alpha - math.pi)
+        body.Fy += F * math.sin(alpha - math.pi)
+    #Если шарик не касается поверхности, то на него действует только сила тяжести
+    else:
+        body.Fx = 0
+        body.Fy = -body.m * g
+    # перемещение тела
+    ax = body.Fx / body.m
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
+  
+    oldy = body.y
+    ay = body.Fy / body.m
+    body.Vy += ay * dt
+    body.y += body.Vy * dt
+    # проверка и коррекция при выходе тела за плоскость 
+    x0 = body.x
+    y0 = body.y
+    r = body.r
+    alpha = surface.find_alpha(x0)
+    x = math.sin(alpha) * r + x0
+    y = surface.find_y(x)
+    if (x0 - x) ** 2 + (y0 - y) ** 2 < r ** 2:
+        body.y = (r ** 2 - (x0 - x) ** 2) ** 0.5 + y
+        body.Vy = (body.y - oldy) / dt
+    if x0 + r > max_x:
+        body.x = max_x - r
+        body.Vx = 0
+    if y0 - r < 0:
+        body.y = r
+        body.Vy = 0
+    if x0 - 2 * r < 0:
+        body.x = 2 * r
+        body.Vx = 0
---
+++ BMS_surface.py
@@ -0,0 +1,110 @@
+# coding: utf-8
+# license: GPLv3
+import math
+'''
+Содержит класс поверхностей
+'''
+class point:
+    ''' 
+    Класс, описывающий точки поверхности
+    Содержит координаты точки, тип (подвижная) цвет и коэффициент трения
+    '''
+    def __init__(self, x = 0, y = 0, friction = 0, color = 'black'):
+        '''
+        Инициализация класса точки
+        '''
+        # координаты точки
+        self.x = x
+        self.y = y
+        # коэффициент трения, который действует на участке поверхности, начиная с данной точки.
+        self.friction = friction
+        # цвет
+        self.color = color
+
+class surface:
+    '''
+    Класс, описывающий поверхности, по которой двигается шар
+    Содержит точки, ограничивающие прямые участки поверхности
+    '''
+    def __init__(self):
+        '''
+        Инициализация класса траектории: пустой список точек
+        По умолчанию черный цвет и линейная интерполяция
+        '''
+        # точки 
+        self.points = []
+        self.lines = []
+       
+
+        
+    def addpoint(self, newpoint):
+        '''
+        Добавление точки поверхности.
+        Поверхность не может иметь точки с одинаковыми координатами по оси Х, поэтому, если новая точка имеет дубль по оси Х, то она не добавляется.
+        Точки поверхности всегда упорядочены по оси Х, поэтому после добавления точки производится сортировка
+        '''
+        #Проверка дубликатов
+        find = False
+        for a in self.points:
+            if a.x==newpoint.x:
+                find = True
+        if not find:
+            #Добавление точки в список
+            self.points.append(newpoint)
+            #Сортировка списка точек по Х
+            self.points.sort(key = lambda points: points.x)
+    
+    def find_y(self, x):
+        '''
+        Определение координаты У по координате Х для любой точки поверхности на основе линейной интерполяции 
+        '''
+        n = len(self.points)
+        # если количество точек менее 2 или Х выходит за диапазон поверхности, то функция возвращает -1
+        y = -1
+        if n > 2 and x >= self.points[0].x and x <= self.points[n - 1].x:
+            # поиск точки с абсциссой Х или интервала, в котором лежит точка
+            for i in range(0, n - 1):
+                if x == self.points[i].x:
+                    y = self.points[i].y
+                #если найден интервал, то вычисляется координата точки на основе интерполяции
+                
+                elif x > self.points[i].x and x < self.points[i + 1].x:
+                    k = (self.points[i + 1].y - self.points[i].y) / (self.points[i + 1].x - self.points[i].x)
+                    b = self.points[i].y - self.points[i].x * k
+                    y = k * x + b
+            if y == -1 and x == self.points[n - 1].x:
+                y = self.points[n - 1].y                        
+        return y
+
+    def find_friction(self, x):
+        '''
+        Определение коэффициента трения по координате Х для любой точки поверхности на основе линейной интерполяции 
+        '''
+        n = len(self.points)
+        # если количество точек менее 2 или Х выходит за диапазон поверхности, то функция возвращает -1
+        kf = 0
+        if n > 2 and x >= self.points[0].x and x <= self.points[n - 1].x:
+            # поиск точки с абсциссой Х или интервала, в котором лежит точка
+            for i in range(0, n - 1):
+                if x >= self.points[i].x and x < self.points[i + 1].x:
+                    kf = self.points[i].friction
+        return kf
+
+    def find_alpha(self, x):
+        '''
+        Функция вычисляет угол касательной к кривой в точке с координатой Х 
+        '''
+        n = len(self.points)
+        # если количество точек менее 2 или Х выходит за диапазон поверхности, то функция возвращает 0
+        alpha = math.pi / 2
+        if n > 2 and x >= self.points[0].x and x <= self.points[n - 1].x:
+            if self.points[0].x == x:
+                alpha = math.atan2(self.find_y(self.points[0].x + 0.01) - self.points[0].y, 0.01)
+            elif self.points[n - 1].x == x:
+                alpha = math.atan2(self.points[n - 1].y - self.find_y(self.points[n - 1].x - 0.01), 0.01)
+            else:
+                alpha = math.atan((self.find_y(x + 0.01) - self.find_y(x - 0.01))/0.02)
+        return alpha             
+
+
+    
